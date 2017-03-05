b9a5bf5bf92cb9ccf1052adcb4e28075f02afa71
Sara Kristjánsdóttir <sarakristjansdottir@Saras-MacBook-Air.local>
2017-03-05 16:22:07 +0000

    display exercise text

--- game.js
+++ game.js
@@ -1,9 +1,20 @@
-// create instance of the game
+// Create instance of the game
 var width = 900;
 var height = 600;
 var game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update}, true);
 
-// load the resources needed
+// Create a variable for bitMapData to display text, used in functions Assignment and KeyPress
+var textArea;
+// Define the size of the area
+var textAreaX = 900;
+var textAreaY = 65;
+
+// Variables for the assignments text
+var text = "carlos carlos";
+var textX = 450;
+var textY = 50;
+
+// Load the resources needed
 function preload () 
 {
 	// Background images
@@ -25,12 +36,12 @@ function update()
 
 }
 
-// load the home page
+// Load the home page
 function loadHomePage() 
 {
 	var homePage = game.add.image(game.world.centerX, game.world.centerY, 'homePage');
 	homePage.anchor.setTo(0.5, 0.5);
-	// add offset of 4px to remove black frame
+	// Add offset of 4px to remove black frame
     homePage.width = width + 4;
     homePage.height = height + 4;
 
@@ -41,10 +52,22 @@ function loadHomePage()
 
 function Assignment() 
 {
-	// remove the background from the homepage
+	// Empty the canvas
    	game.world.removeAll();
 
+   	// Load new background
     background = game.add.image(game.world.centerX, game.world.centerY, 'homeKeysBackground');
     background.anchor.setTo(0.5, 0.5);
+
+    // Create the textArea
+    textArea = game.make.bitmapData(game.world.width, textAreaY);
+    
+    // Add style to the textArea
+    textArea.context.font = '64px Arial';
+    textArea.context.fillStyle = '#ffffff';
+
+    // Display the text
+    textArea.context.fillText(text, textX, textY);
+    textArea.addToWorld();
 
 }
