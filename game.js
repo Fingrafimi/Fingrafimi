263ba652e8f98e921ae22be38a0021a800db61d7
Sara Kristjánsdóttir <sarakristjansdottir@Saras-MacBook-Air.local>
2017-03-05 15:43:06 +0000

    create instance of the game

---
+++ game.js
@@ -0,0 +1,18 @@
+// create instance of the game
+var game = new Phaser.Game(900, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update}, true);
+
+// load the resources needed
+function preload () 
+{
+
+}
+
+function create () 
+{
+
+}
+
+function update()
+{
+
+}
