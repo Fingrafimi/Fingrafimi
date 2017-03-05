96664ff210db1fd0ed6024c9305324eceb22f0e5
Sara Kristjánsdóttir <sarakristjansdottir@Saras-MacBook-Air.local>
2017-03-05 15:51:52 +0000

    load background for home page

--- game.js
+++ game.js
@@ -1,18 +1,32 @@
 // create instance of the game
-var game = new Phaser.Game(900, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update}, true);
+var width = 900;
+var height = 600;
+var game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update}, true);
 
 // load the resources needed
 function preload () 
 {
+	game.load.image('homePage', 'Assets/homePage.bmp');
 
 }
 
 function create () 
 {
-
+	loadHomePage();
 }
 
 function update()
 {
+
+}
+
+// load the home page
+function loadHomePage() 
+{
+	var homePage = game.add.image(game.world.centerX, game.world.centerY, 'homePage');
+	homePage.anchor.setTo(0.5, 0.5);
+	// add offset of 4px to remove black frame
+    homePage.width = width + 4;
+    homePage.height = height + 4;
 
 }
