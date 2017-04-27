8f0f4433c25fe014df7122014641d88170117856
Sara <sarakri13@ru.is>
2017-04-27 10:35:00 +0000

    refactoring

--- game.js
+++ game.js
@@ -159,25 +159,28 @@ function create()
 
 function update()
 {
-    if(warmupHead.angle > -46 && warmupHead.x > 1015)
+    if(inTutorial)
     {
-        warmupHead.x -= 2;
-        warmupHead.angle -= 1;
-    }
+        if(warmupHead.angle > -46 && warmupHead.x > 1015)
+        {
+            warmupHead.x -= 2;
+            warmupHead.angle -= 1;
+        }
 
-    /*if(warmupHead.angle < -40)
-    {
-        balloon.visible = true;       
-    }*/
+        /*if(warmupHead.angle < -40)
+        {
+            balloon.visible = true;       
+        }*/
 
-    if(leftHand.y > 320 && balloon.visible === true)
-    {
-        leftHand.y -= 5;
-    }
+        if(leftHand.y > 320 && balloon.visible === true)
+        {
+            leftHand.y -= 5;
+        }
 
-    if(rightHand.y > 320 && balloon.visible === true)
-    {
-        rightHand.y -= 5;
+        if(rightHand.y > 320 && balloon.visible === true)
+        {
+            rightHand.y -= 5;
+        }
     }
 
 }
@@ -429,6 +432,7 @@ function addExitButton()
     exitBtn.events.onInputOut.add(outExit);
     exitBtn.events.onInputDown.add(loadHomePage);
     exitBtn.events.onInputDown.add(quitExercise);
+
 }
 
 function quitExercise()
@@ -794,38 +798,43 @@ function WarmUpFJ(assignmentNr, exerciseNr)
             warmupHead.animations.stop(); 
             warmupHead.frame = 0;
 
-            //fogj2 is the soundclip where he says "Finndu stafina J, K, L og Æ"
-            sounds['fogj2'].onStop.add(function(){
-                            //Make Maggi stop moving his mouth in the 2 second pause between animations 
-                            warmupHead.animations.stop(); 
-                            warmupHead.frame = 0;
-                            //Pause for 2 seconds, then play the soundclip "Finndu stafina F og J" and make both F and J blink
-                            game.time.events.add(Phaser.Timer.SECOND * 2, function(){
-                                if(inTutorial)
-                                {
-                                    //Make Maggi talk, blink both F and J, set correct text in speech bubble and play soundclip
-                                    warmupHead.play('talk');
-                                    warmupKeys.play('bothBlink');
-                                    balloon.frame = 4;
-                                    sounds['findFJ'].play();
-                                } 
-                            }, this).autoDestroy = true;  
-                        }, this);
+            
             //Pause for 2 seconds after fogj1 soundclip, then play fogj2
             game.time.events.add(Phaser.Timer.SECOND * 2, function(){
-                    if(inTutorial)
-                    {
-                        //When fogj2 soundclip starts, make Maggi talk, put the correct text in speech bubble, make J, K, L and Æ blink 
-                        //and add the right hand into the game so it can start moving up towards the keys.
-                        warmupHead.play('talk');
-                        sounds['fogj2'].play();
-                        warmupKeys.play('rightBlink');
-                        balloon.frame = 2;
-                        rightHand = game.add.sprite(535, 700, 'rHand', 0);
-                        rightHand.scale.setTo(1.1);
-                    }
+               
+                //When fogj2 soundclip starts, make Maggi talk, put the correct text in speech bubble, make J, K, L and Æ blink 
+                //and add the right hand into the game so it can start moving up towards the keys.
+                warmupHead.play('talk');
+                sounds['fogj2'].play();
+                warmupKeys.play('rightBlink');
+                balloon.frame = 2;
+                rightHand = game.add.sprite(535, 700, 'rHand', 0);
+                rightHand.scale.setTo(1.1);
+                  
             }, this).autoDestroy = true;  
     }, this);
+
+    //fogj2 is the soundclip where he says "Finndu stafina J, K, L og Æ"
+     sounds['fogj2'].onStop.add(function(){
+            //Make Maggi stop moving his mouth in the 2 second pause between animations 
+            warmupHead.animations.stop(); 
+            warmupHead.frame = 0;
+            //Pause for 2 seconds, then play the soundclip "Finndu stafina F og J" and make both F and J blink
+            game.time.events.add(Phaser.Timer.SECOND * 2, function(){
+                          
+            //Make Maggi talk, blink both F and J, set correct text in speech bubble and play soundclip
+            warmupHead.play('talk');
+            warmupKeys.play('bothBlink');
+            balloon.frame = 4;
+            sounds['findFJ'].play();
+                            
+            }, this).autoDestroy = true;  
+    }, this);
+
+    sounds['findFJ'].onStop.add(function(){
+        warmupHead.animations.stop(); 
+        warmupHead.frame = 0;
+    })
     //Play soundclip fogj1, make Maggi talk and make A, S, D and F blink.
     sounds['fogj1'].play();
     warmupHead.play('talk');
