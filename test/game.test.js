58fb1262bcaf3b3696e6d329f9f1d0c73fc8e6df
Carlos RAgnar <carlosk12@ru.is>
2017-05-08 20:56:51 +0000

    Testing test

--- test/game.test.js
+++ test/game.test.js
@@ -1,57 +1,62 @@
-var jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-const {window} = new JSDOM("<!DOCTYPE html>");
-//JSDOM.fromFile("../index.html");
-//const {window}= new JSDOM("<!DOCTYPE html><html><head><script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script></head><body></body></html>");
-//var window = document.defaultView;
-//window = document.createWindow();
+QUnit.test("First Test with Phaser", function( assert ) {
+    
+    assert.equal( value, "1", "Value should be one");
+});
 
-//var window = jsdom.defaultView;
-var jquery = require('jquery');
+// /*var jsdom = require('jsdom');
+// const { JSDOM } = jsdom;
+// const {jsdom2} = new JSDOM("<!DOCTYPE html>");
+// //JSDOM.fromFile("../index.html");
+// //const {window}= new JSDOM("<!DOCTYPE html><html><head><script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script></head><body></body></html>");
+// //var window = document.defaultView;
+// //window = document.createWindow();
 
-
-global.window = window;
-global.navigator = window.navigator;
-
-global.document = window.document;
-global.Element = window.element;
-
-//global.$ = jQuery(window.defaultView);
+// //var window = jsdom.defaultView;
+// //var jquery = require('jquery');
 
 
+// global.window = window;
+// global.navigator = window.navigator;
 
-var Canvas = require('canvas');
-global.Image = Canvas.Image;
-PIXI = require('./pixi.js');
-p2 = require('./p2.js');
-Phaser = require('../phaser.min.js');
-exercises = require('../exercises.js');
+// global.document = window.document;
+// global.Element = window.element;
 
-game = require('../game.js');
-console.log("bla");
-console.log(game);
-console.log(exercises);
+// //global.$ = jQuery(window.defaultView);
 
 
-//exec('../game.js');
 
-QUnit.test("Initialize Text variables1", function( assert ) {
-   /*game.text = "someText";
-    game.corrCount = 1;
-    game.inCorrPos = 1;
-    Game.textPos = 1;*/
-    var text ="hello";
-    assert.equal(game.addBalloon(0), 3);
-  });
+// var Canvas = require('canvas');
+// global.Image = Canvas.Image;
+// PIXI = require('./pixi.js');
+// p2 = require('./p2.js');
+// Phaser = require('../phaser.min.js');
+// exercises = require('../exercises.js');
 
-QUnit.test("Initialize Text variables", function( assert ) {
-   /*game.text = "someText";
-    game.corrCount = 1;
-    game.inCorrPos = 1;
-    Game.textPos = 1;*/
-    game.initTextVariables();
-    assert.equal(text, "");
-    assert.equal(corrCount, 0);
-    assert.equal(incorrPos, -1);
-    assert.equal(textPos, 0);
-  });
+// game = require('../game.js');
+// console.log("bla");
+// console.log(game);
+// console.log(exercises);
+
+
+// //exec('../game.js');
+
+// QUnit.test("Initialize Text variables1", function( assert ) {
+//    /*game.text = "someText";
+//     game.corrCount = 1;
+//     game.inCorrPos = 1;
+//     Game.textPos = 1;*/
+//     var text ="hello";
+//     assert.equal(game.addBalloon(0), 3);
+//   });
+
+// QUnit.test("Initialize Text variables", function( assert ) {
+//     game.text = "someText";
+//     game.corrCount = 1;
+//     game.inCorrPos = 1;
+//     Game.textPos = 1;
+//     //game.initTextVariables();
+//     assert.equal(text, "");
+//     assert.equal(corrCount, 0);
+//     assert.equal(incorrPos, -1);
+//     assert.equal(textPos, 0);
+//   });
